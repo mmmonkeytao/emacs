045fccaf77c06ae1325aa7493d2305b5c55ef204
Ye Tao <taoyeandy@Andy-Tao.local>
2015-02-21 00:19:13 +0100

    add auto complete package, yasnippet and popup-complte

--- config/custom.el
+++ config/custom.el
@@ -30,3 +30,19 @@
 ;;;;;;;; iedit ;;;;;
 (add-to-list 'load-path "~/.emacs.d/elpa/iedit-0.9")
 (define-key global-map (kbd "C-c ;") 'iedit-mode)
+
+;; popup auto complete package
+(add-to-list 'load-path "~/.emacs.d/elpa/popup-0.5.2/")
+(require 'popup)
+
+;; ;;;;; auto complete package ;;;;;;
+(add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-1.5.0/")
+(require 'auto-complete)
+;;  ;; do default config for auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; start yasnippet with emacs
+(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-0.9.0.1")
+(require 'yasnippet)
+(yas-global-mode 1)
